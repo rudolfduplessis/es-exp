7c9611ebfadfefbc9fa3e64540843f99f4bf8b03
Rudolf <rudolf@ordercloud.co.za>
2017-09-08 10:35:38 +0200

    Some more progress

--- src/main/resources/db/migration/V1501878437__seed.sql
+++ src/main/resources/db/migration/V1501878437__seed.sql
@@ -4,10 +4,17 @@ BEGIN
 
   IF NOT exists(SELECT * FROM information_schema.tables WHERE table_schema = 'event_schema' AND table_name = 'event') THEN
     CREATE TABLE event_schema.event(
+      event_id UUID NOT NULL,
+      event_name TEXT NOT NULL,
+      event_raised TIMESTAMP NOT NULL,
+      event_applies TIMESTAMP NOT NULL,
+      event_description TEXT,
+      event_sender_id UUID NOT NULL,
+      aggregate_id UUID NOT NULL,
       event JSONB NOT NULL,
       instance UUID NOT NULL
     );
-    CREATE INDEX event_instance_idx ON event_schema.event (instance);
+    CREATE INDEX event_instance_aggregate_idx ON event_schema.event (instance, aggregate_id, event_raised, event_applies);
   END IF;
 
   IF NOT exists(SELECT * FROM information_schema.tables WHERE table_schema = 'event_schema' AND table_name = 'snapshot') THEN
